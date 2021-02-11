0bb7f72b6536709302142acbd0b2877a8017a1f3
kyriox <job@Powers-iMac.local>
2021-02-10 21:00:36 -0600

    tokenizador simple

--- SimpleNLP.py
+++ SimpleNLP.py
@@ -3,7 +3,7 @@ import scipy.sparse as sp
 # usaremos la variable derivador y podra tomar los valores de "lemma", "stem" o None
 class Tokenizer:
     def __init__(self,corpus=[],ngrams=[],case_folding=True, derivador=None):
-        self.ngrams.append(1)
+        self.ngrams=ngrams
         self.vocabulario={}
         self.vocabulario_index=[]
         if corpus:
@@ -17,12 +17,30 @@ class Tokenizer:
     def _construye_vocabulario(self,corpus):
         ## Aplicar normalizaciones al corpus
         ## construir tabla de vocabulario
-        pass
+        self.vocabulario={}
+        self.vocabulario_index=[]
+        norm_corpus=self._case_folding(corpus)
+        for doc in norm_corpus:
+            for token in doc:
+                if token not in self.vocabulario_index:
+                    self.vocabulario_index.append(token)
+                id_token=self.vocabulario_index.index(token)
+                self.vocabulario[id_token]=self.vocabulario.get(id_token,0)+1
   
     def fit(self, corpus):
            self._construye_vocabulario(corpus) 
             
-    def transform(self, corpus):
+    def transform(self, texts):
+        norm_texts=self._case_folding(texts)
+        vectores=[]
+        N=len(self.vocabulario_index)
+        for doc in norm_texts:
+            C=[self.vocabulario_index.index(token) for token in doc if token in self.vocabulario_index]
+            nt=len(C)
+            X=[1 for i in range(nt)]
+            R=[0 for i in range(nt)]
+            vectores.append(sp.csr_matrix((X,(R,C)), shape=(1,N)))
+        return sp.vstack(vectores)
         ## Aplicar normalizaciones al texto
         ## Tokenizar el texto utilizando el vocabulario 
         #(decidir que hacer cuando ocurren palabras que no se encuentres en el vocabulario)
